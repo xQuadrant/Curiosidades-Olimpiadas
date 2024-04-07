a97017a33d63e0a07c03471dbb01508a87f92483
joaomarcelo15 <164264383+joaomarcelo15@users.noreply.github.com>
2024-04-07 17:01:13 -0300

    questoes 4 e 5

--- pf site.js
+++ pf site.js
@@ -34,6 +34,13 @@ const filtroQuestao2 = (linha, pais) => {
 const filtroQuestao3 = (linha, ano) => {
     return linha?.Season === 'Winter' && linha?.Year === ano // Filtro de estação e ano.
 }
+const filtroQuestao4 = (linha, idade) => {
+    return linha?.Season === 'Winter' && linha.Age  === idade // Filtro de estação e país.
+}
+const filtroQuestao5 = (linha, sexo) => {
+    return linha?.Season === 'Winter' && linha?.Sex === sexo // Filtro de estação e sexo.
+}
+
 // As funções buscarQ1, buscarQ2, etc., chamam a função lerCSV com os filtros apropriados e retornam os resultados de acordo com a questão específica. Cada uma delas recebe os parâmetros necessários para aplicar o filtro específico.
 // Estas funções tem o funcionamento igual e argumentos praticamente idênticos, por tanto não é necessário a explicação de todas elas, apenas uma.
 // Retorno questão 1
@@ -62,3 +69,19 @@ const buscarQ3 = (ano) => lerCSV(
     [],
     results => results[0].City // Este é o único diferencial entre as funções. Esta linha em específico retorna APENAS a primeira cidade a ser encontrada no processamento.
     );
+// Retorno questão 4
+const buscarQ4 = (idade) => lerCSV(// A função recebe um parâmetro,idade e chama a função "lerCSV".
+    'athlete_events.csv',// Caminho do arquivo.
+    filtroQuestao4,// Função de filtro.
+    [idade],// Argumentos do filtro
+    [],// Lista vazia, para acúmulo de resultados.
+    results => results.length// Comprimento da lista de resultados.
+    );
+
+// Retorno questão 5
+const buscarQ5 = (sexo) => lerCSV(
+    'athlete_events.csv',
+    filtroQuestao5,
+    [sexo],
+    [],
+    results => results.length);
